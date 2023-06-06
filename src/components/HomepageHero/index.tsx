6fe790c0ab389c1ce21f1a55a37a5191cba54dc0
bbuilds <hello@brandenbuilds.com>
2023-06-06 10:18:15 -0500

    refactor: :recycle: setup translations for homepage hero

--- src/components/HomepageHero/index.tsx
+++ src/components/HomepageHero/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import clsx from "clsx";
+import Translate from "@docusaurus/Translate";
 import HeroBg from "./HeroBgAnimation";
 import styles from "./styles.module.scss";
 
@@ -16,16 +17,20 @@ export default function HomepageHero(): JSX.Element {
         <div className="row">
           <div className="col col--10">
             <h1 className={clsx("type-alpha")}>
-              An open-source identity solution built for the open internet
+              <Translate id="homepage.heroTitle">
+                An open-source identity solution built for the open internet
+              </Translate>
             </h1>
           </div>
         </div>
         <div className="row">
           <div className="col col--6">
             <p className={clsx("type-paragraph-large")}>
-              Unified ID 2.0 leverages encrypted email and phone number data to
-              provide a privacy-conscious, secure, and accurate identity
-              standard for the entire digital advertising ecosystem.
+              <Translate id="homepage.heroCopy">
+                Unified ID 2.0 leverages encrypted email and phone number data
+                to provide a privacy-conscious, secure, and accurate identity
+                standard for the entire digital advertising ecosystem.
+              </Translate>
             </p>
           </div>
         </div>
