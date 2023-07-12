79ad18e687377c796711586ca122a4967317fb95
Lionell Pack <lionell.pack@thetradedesk.com>
2023-07-12 15:38:49 +1000

    Create a file to override some docusaurus config in the preview only.

---
+++ docusaurus.preview.js
@@ -0,0 +1,22 @@
+import * as original from './docusaurus.original.js';
+
+/*
+ * N.B. This file is used for the preview site. 
+ * `docusaurus.config.js` is renamed to `docusaurus.original.js`, this file is renamed to `docusaurus.config.js`, and
+ * then this file is included (which will import the original file and update the settings for the preview site).
+*/
+
+config = {
+  ...original.default,
+  url: "https://unifiedid2.github.io/",
+  baseUrl: "/uid2-docs-preview/",
+  organizationName: "UnifiedID2", // Usually your GitHub org/user name.
+  projectName: "uid2-docs-preview", // Usually your repo name.
+  themeConfig: {
+    ...original.themeConfig,
+    algolia: undefined
+  }
+};
+
+
+module.exports = config;
