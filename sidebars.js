5a1a30598014cb99f1628726862e48a1ddbbb691
genwhittTTD <gen.whitt@thetradedesk.com>
2025-01-28 11:50:30 -0500

    remove test file + restore sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -75,7 +75,7 @@ const fullSidebar = [
       label: 'Integration Guides',
       link: {
         type: 'doc',
-        id: 'guides/integration-mobile-overview-TEST',
+        id: 'guides/summary-guides',
       },
       collapsed: false,
 
