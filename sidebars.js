08f17acc565d61bfb149fd9630b7ee773eb6c2ae
Sunny Wu <sunny.wu@thetradedesk.com>
2025-12-02 14:38:19 +1100

    reverted labels changes but added back the keys

--- sidebars.js
+++ sidebars.js
@@ -120,7 +120,7 @@ const fullSidebar = [
                 {
                   type: 'category',
                   label: 'Prebid',
-                  key: 'Prebid-Web-Only-Guides'
+                  key: 'Prebid-Web-Only-Guides',
                   link: {
                     type: 'doc',
                     id: 'guides/integration-prebid',
@@ -170,7 +170,7 @@ const fullSidebar = [
           {
             type: 'category',
             label: 'Prebid',
-            key: 'Prebid-Web-And-Mobile-Guides'
+            key: 'Prebid-Web-And-Mobile-Guides',
             link: {
               type: 'doc',
               id: 'guides/integration-prebid',
